4634431c77e868b2f6d370b03a2f46dc8b981b7e
1607albert <73102135+1607albert@users.noreply.github.com>
2021-04-14 20:24:57 +0200

    commit

--- app/js/map.js
+++ app/js/map.js
@@ -1,129 +1,42 @@
-// @formatter:off
-require([
-        "esri/map",
-        "esri/layers/ArcGISDynamicMapServiceLayer",
-        "esri/layers/FeatureLayer",
+require(["esri/Map", "esri/layers/GeoJSONLayer", "esri/views/MapView"], function (
+    Map,
+    GeoJSONLayer,
+    MapView
+  ) {
+    // If GeoJSON files are not on the same domain as your website, a CORS enabled server
+    // or a proxy is required.
+    const url = "Export_geometry.geojson";
 
-        "esri/symbols/SimpleFillSymbol",
-        "esri/symbols/SimpleLineSymbol",
-        "esri/symbols/SimpleMarkerSymbol",
-        "esri/Color",
+    // Paste the url into a browser's address bar to download and view the attributes
+    // in the GeoJSON file. These attributes include:
+    // * mag - magnitude
+    // * type - earthquake or other event such as nuclear test
+    // * place - location of the event
+    // * time - the time of the event
+    // Use the Arcade Date() function to format time field into a human-readable format
 
-        "dojo/ready",
-        "dojo/parser",
-        "dojo/on",
-        "dojo/dom",
+       
 
+    // const geojsonLayer = new GeoJSONLayer({
+    //   url: url,
+    //   copyright: "USGS Earthquakes",
+    //   popupTemplate: template,
+     
+    // });
+    const geojsonLayer = new GeoJSONLayer({
+        url: url
+         
+      });
 
-        "dojo/_base/declare",
-        "dojo/_base/array",
+    const map = new Map({
+        basemap: "gray-vector",
+        layers: [geojsonLayer]
+      });
 
-        "dijit/layout/BorderContainer",
-        "dijit/layout/ContentPane",
-        "dijit/form/Button"],
-    function (Map, ArcGISDynamicMapServiceLayer, FeatureLayer,
-              SimpleFillSymbol, SimpleLineSymbol, SimpleMarkerSymbol, Color,
-              ready, parser, on, dom,
-              declare, array,
-              BorderContainer, ContentPane, Button) {
-// @formatter:on
-
-        // Wait until DOM is ready *and* all outstanding require() calls have been resolved
-        ready(function () {
-
-            // Parse DOM nodes decorated with the data-dojo-type attribute
-            parser.parse();
-
-            // URL variables
-            var sUrlUSAService = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer";
-            var sUrlQuakesLayer = "Export_geometry.geojson";
-
-            // Create the map
-            var mapMain = new Map("divMap", {
-                basemap: "satellite",
-                center: [-119.65, 36.87],
-                zoom: 4
-            });
-
-            // Construct and wire a button to apply the renderer
-            mapMain.on("layers-add-result", function () {
-                var btnApplyRenderer = new Button({
-                    label: "Show County Population Density",
-                    onClick: changeCountiesRenderer
-                }, "progButtonNode");
-
-                //update earthquakes using a renderer
-                changeQuakesRenderer();
-
-            });
-
-
-            // Construct the USA layer
-            var lyrUSA = new ArcGISDynamicMapServiceLayer(sUrlUSAService, {
-                opacity: 0.5
-            });
-            lyrUSA.setVisibleLayers([0, 1, 3]);
-
-
-            var outFieldsQuakes = ["EQID", "UTC_DATETIME", "MAGNITUDE", "PLACE"];
-
-            // Construct the Quakes layer
-            var lyrQuakes = new FeatureLayer(sUrlQuakesLayer, {
-                outFields: outFieldsQuakes
-
-
-            });
-            lyrQuakes.setDefinitionExpression("MAGNITUDE >= 2.0");
-            mapMain.addLayers([lyrUSA, lyrQuakes]);
-
-
-            function changeQuakesRenderer() {
-
-                // construct a  symbol for earthquake features
-                var quakeSymbol = new SimpleMarkerSymbol();
-                quakeSymbol.setColor(new Color([255, 0, 0, 0.5]));
-                quakeSymbol.setOutline(null);
-
-
-                /*
-                 * Step: Construct and apply a simple renderer for earthquake features
-                 */
-
-
-                /*
-                 * Step: Construct symbol size info parameters for the quake renderer
-                 */
-
-
-                /*
-                 * Step: Apply symbol size info to the quake renderer
-                 */
-
-
-            }
-
-
-            function changeCountiesRenderer() {
-
-                var symDefault = new SimpleFillSymbol().setColor(new Color([255, 255, 0]));
-
-                /*
-                 * Step: Construct a class breaks renderer
-                 */
-
-
-                /*
-                 * Step: Define the class breaks
-                 */
-
-
-                /*
-                 * Step: Apply the renderer to the Counties layer
-                 */
-
-
-            }
-
-
-        });
-    });
+      const view = new MapView({
+        container: "viewDiv",
+        center: [-168, 46],
+        zoom: 2,
+        map: map
+      });
+  });
